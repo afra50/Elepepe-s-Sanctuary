fd45a2470d5787380070290b1098a7a19a100fc6
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-08-12 22:43:20 +0200

    szkielet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./styles/App.scss";
+import routes from "./routes";
 
-function App() {
+
+function AppRoutes() {
+  return useRoutes(routes);
+}
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+          <AppRoutes />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
-export default App;
