6f1373deee6be73595935d25ff910e0a3db5ad64
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-12-03 00:38:39 +0100

    tlumaczenia kontakt

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -7,14 +7,16 @@ import esHeader from "./locales/es/header.json";
 import enFooter from "./locales/en/footer.json";
 import plFooter from "./locales/pl/footer.json";
 import esFooter from "./locales/es/footer.json";
-
+import enContact from "./locales/en/contact.json";
 import plContact from "./locales/pl/contact.json";
+import esContact from "./locales/es/contact.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: {
       header: enHeader,
       footer: enFooter,
+      contact: enContact,
     },
     pl: {
       header: plHeader,
@@ -24,6 +26,7 @@ i18n.use(initReactI18next).init({
     es: {
       header: esHeader,
       footer: esFooter,
+      contact: esContact,
     },
   },
   lng: "en",
