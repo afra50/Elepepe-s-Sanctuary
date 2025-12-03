24ffe3b294bc932a28c1e03dc2e4d4dad0d4c264
Julia Kowalczyk <jkowalczyk505@gmail.com>
2025-12-03 22:50:41 +0100

    o nas plik

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,9 +1,11 @@
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
 
 const routes = [
   { path: "/", element: <HomePage /> },
   { path: "/contact", element: <ContactPage /> },
+  { path: "/about", element: <AboutPage /> },
 ];
 
 export default routes;
