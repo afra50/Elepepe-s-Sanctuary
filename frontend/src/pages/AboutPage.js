42c551069b7db59272b772796a12c0505b8d5151
Chris <chrisoo1991@gmail.com>
2025-12-04 01:14:13 +0100

    dodanie tekstu about us

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,9 +1,46 @@
 function AboutPage() {
-  return (
-    <main className="page-about">
-      <p>test</p>
-    </main>
-  );
+	return (
+		<main className="page-about">
+			<header>
+				<h1 className="page-header">O nas</h1>
+			</header>
+			<p className="page-header">Kim jesteśmy?</p>
+
+			<ul>
+				<li>
+					<strong>Fundacja Elepepe’s Sanctuary</strong> powstała z potrzeby
+					serca – by pomagać szczurom w potrzebie i ich opiekunom, których
+					często nie stać na kosztowne leczenie. Chcemy dotrzeć do tych, którzy
+					najbardziej potrzebują wsparcia, ale nie zawsze wiemy, gdzie ich
+					znaleźć. Dlatego zwracamy się do Was – jeśli znacie szczurka
+					wymagającego pomocy lub właściciela, który nie może sobie poradzić z
+					kosztami opieki, napiszcie do nas.
+				</li>
+				<br />
+				<br />
+
+				<li>
+					<strong>Fundacja </strong> powstała z miłości do jednego szczura –
+					Elepepe. To on nauczył swojego opiekuna, że gryzonie to nie tylko
+					„małe zwierzątka w klatkach”, ale pełne uczuć istoty, które potrafią
+					kochać i zmieniać życie człowieka.Naszym celem jest pomoc szczurkom i
+					świnkom morskim, małym, często niedocenianym istotom, które zasługują
+					na miłość i troskę.
+				</li>
+				<br />
+				<br />
+				<li>
+					<strong>Elepepe’s Sanctuary</strong> to organizacja non-profit, której
+					misją jest wspieranie opiekunów szczurów i świnek morskich w
+					pokrywaniu kosztów leczenia weterynaryjnego, gdy nie są w stanie ich
+					udźwignąć, a zdrowie zwierząt jest zagrożone. Na ten moment naszym
+					głównym celem jest zebranie jak największej liczby osób w grupie, w
+					której można dodawać informacje o potrzebnej pomocy dla zwierząt —
+					swoich lub tych, o których słyszeliście.
+				</li>
+			</ul>
+		</main>
+	);
 }
 
 export default AboutPage;
