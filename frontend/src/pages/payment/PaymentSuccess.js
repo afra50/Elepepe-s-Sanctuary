8ae23d6194d009cfe29285f909ae12c4648df5de
Chris <chrisoo1991@gmail.com>
2026-01-04 01:10:17 +0100

    podstrony płatności

--- frontend/src/pages/payment/PaymentSuccess.js
+++ frontend/src/pages/payment/PaymentSuccess.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { CircleCheckBig } from "lucide-react";
+import Button from "../../components/ui/Button";
+import { NavLink } from "react-router-dom";
 
 function PaymentSuccess() {
-  return (
-    <div>
-      <CircleCheckBig />
-      <h1>Płatność udana</h1>
-      <p>
-        blablablablabalbalbalbalbalbabablababalbalabalbabalba blablaalbalbablbab
-        nddijqdijqidjjqi
-      </p>
-      <div>
-        <a href="/">Wróć do strony głównej</a>
-        <a>Zobacz zbiórkę</a>
-      </div>
-    </div>
-  );
+	return (
+		<div className="last-payment-page">
+			<div className="payment-container">
+				<CircleCheckBig className="success-icon" />
+				<h1 className="last-payment-title">Płatność udana</h1>
+				<p>
+					Dziękujemy za dokonanie płatności. Sprawdź naszą zbiórkę lub wróć na
+					stronę główną.
+				</p>
+				<div className="buttons-container">
+					<NavLink to="/">
+						<Button variant="primary" size="lg">
+							Zobacz zbiórkę
+						</Button>
+					</NavLink>
+					<NavLink to="/">
+						<Button variant="secondary" size="lg">
+							Strona główna
+						</Button>
+					</NavLink>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default PaymentSuccess;
